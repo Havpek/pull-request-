418b0c2967e96d81202ce62dd5e546f44f56e963
Havpek <151518455+Havpek@users.noreply.github.com>
2024-05-24 16:20:06 +0700

    Create lesson_2_task_5.py

---
+++ lesson_2/lesson_2_task_5.py
@@ -0,0 +1,26 @@
+def month_to_season(n):
+    if n ==12:
+        print(n, "Зима")
+    if n ==1:
+        print(n, "Зима")  
+    if n ==2:
+        print(n, "Зима")
+    if n ==3:
+        print(n, "Весна")
+    if n ==4:
+        print(n, "Весна")
+    if n ==5:
+        print(n, "Весна")
+    if n ==6:
+        print(n, "Лето") 
+    if n ==7:
+        print(n, "Лето")  
+    if n ==8:
+        print(n, "Лето")
+    if n ==9:
+        print(n, "Осень")
+    if n ==10:
+        print(n, "Осень")
+    if n ==11:
+        print(n, "Осень")
+print(month_to_season(9))                                 
