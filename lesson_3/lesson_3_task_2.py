707d4f1ab0170b2f03672f959bfe83d3f825bca7
Havpek <151518455+Havpek@users.noreply.github.com>
2024-05-31 17:21:07 +0700

    add

--- lesson_3/lesson_3_task_2.py
+++ lesson_3/lesson_3_task_2.py
@@ -1,6 +1,12 @@
 from smartphone import smartphone
-iPhone = smartphone("iPhone", "15 Pro Max", "+79134672828")
-Samsung = smartphone("Samsung", "S24 Ultra", "+79234232929")
-Redmi =  smartphone("Redmi", "11 Pro+", "+79143222324")
-catalog = [iPhone, Samsung, Redmi]
-print(catalog)
+catalog = []
+phone1 = smartphone("iPhone", "15 Pro Max", "+79134672828")
+phone2 = smartphone("Samsung", "S24 Ultra", "+79234232929")
+phone3 =  smartphone("Redmi", "11 Pro+", "+79143222324")
+
+catalog.append(phone1)
+catalog.append(phone2)
+catalog.append(phone3)
+
+for phone in catalog:
+    print(f"{phone.brand} - {phone.model}. - {phone.phonenumber}")
--- lesson_3/smartphone.py
+++ lesson_3/smartphone.py
@@ -1,6 +1,5 @@
 class smartphone:
-    def __init__(self, brand, model, phonenamber):
-        print("Смартфон", brand, model, phonenamber)
-        self.smartphone = brand, model, phonenamber
-    def seysmartphone(self):
-        print("Смартфон", self.smartphone)
+    def __init__(self, brand, model, phonenumber):
+        self.brand = brand
+        self.model = model
+        self.phonenumber = phonenumber
