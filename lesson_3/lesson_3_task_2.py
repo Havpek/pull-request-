373acf8abc4b75135030d63b5c08f9a9924eabc2
Havpek <151518455+Havpek@users.noreply.github.com>
2024-05-31 14:52:58 +0700

    add

---
+++ lesson_3/lesson_3_task_2.py
@@ -0,0 +1,6 @@
+from smartphone import smartphone
+iPhone = smartphone("iPhone", "15 Pro Max", "+79134672828")
+Samsung = smartphone("Samsung", "S24 Ultra", "+79234232929")
+Redmi =  smartphone("Redmi", "11 Pro+", "+79143222324")
+catalog = [iPhone, Samsung, Redmi]
+print(catalog)
---
+++ lesson_3/smartphone.py
@@ -0,0 +1,6 @@
+class smartphone:
+    def __init__(self, brand, model, phonenamber):
+        print("Смартфон", brand, model, phonenamber)
+        self.smartphone = brand, model, phonenamber
+    def seysmartphone(self):
+        print("Смартфон", self.smartphone)
