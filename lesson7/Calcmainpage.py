6c31271f4d766627d8ca399ec0cab36b4f0890b0
Havpek <151518455+Havpek@users.noreply.github.com>
2024-07-14 07:55:14 +0600

    change

--- lesson7/Calcmainpage.py
+++ lesson7/Calcmainpage.py
@@ -24,5 +24,5 @@ class CalcMain:
 
 
     def wait_button_gettext(self):
-        WebDriverWait(self.browser, 46).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "screen") "15")
+        WebDriverWait(self.browser, 46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"),"15"))
         return self.browser.find_element(By.CLASS_NAME, "screen").text
